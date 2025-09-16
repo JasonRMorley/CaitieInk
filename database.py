3465502a84f4207689c5c1e41613bd40019381bf
Jason Morley <jrmorley@live.co.uk>
2025-09-16 13:28:31 +0100

    requirements.txt updated

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL', os.getenv("DATABASE_URL_LOCAL"))
 
 engine = create_engine(DATABASE_URL)
 SessionFactory = sessionmaker(bind=engine, expire_on_commit=False)
